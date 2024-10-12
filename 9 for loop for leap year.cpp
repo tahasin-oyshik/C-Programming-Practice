d7b3484cc1af77399ca1ff74282a2ccd18c8cf3b
tahasin-oyshik <tahasin.oyshik@gmail.com>
2024-10-13 03:16:45 +0600

    9 format change

--- 9 for loop for leap year.cpp
+++ 9 for loop for leap year.cpp
@@ -9,34 +9,25 @@ int main ()
 {
     int i;
 
-    for (i=2000;i<2026;i++)
+    for (i=2000; i<2026; i++)
     {
-
         if (i%400==0)
         {
-        printf ("%d is a leap year\n",i);
-
+            printf ("%d is a leap year\n",i);
         }
         else if (i%100==0 && i%400!=0)
         {
-        printf ("%d isn't a leap year\n",i);
-
+            printf ("%d isn't a leap year\n",i);
         }
         else if (i%400!=0 && i%100!=0 && i%4==0)
         {
-        printf ("%d is a leap year\n",i);
-
+            printf ("%d is a leap year\n",i);
         }
         else
         {
-        printf ("%d isn't a leap year\n",i);
+            printf ("%d isn't a leap year\n",i);
 
         }
-
     }
-
-
-
-
     return 0;
 }
